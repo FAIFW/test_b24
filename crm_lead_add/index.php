d629b935eed6842e5bd80302b539f1d758b23b66
fa1fw <fa1994f.w@gmail.com>
2024-09-19 14:44:47 +0300

    test_2_creations

---
+++ crm_lead_add/index.php
@@ -0,0 +1,18 @@
+<?
+require_once (__DIR__.'/crest.php');
+
+//that was working:
+$result = 'hello world';
+
+/*
+$result = CRest::call(
+		'crm.lead.add',
+		{{query[params][PARAMS]}}
+	);
+*/
+
+
+
+echo '<pre>';
+	print_r($result);
+echo '</pre>';
