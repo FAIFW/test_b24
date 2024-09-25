4cd60f167cdf7a482d866a1a8491d196cf12a004
fa1fw <fa1994f.w@gmail.com>
2024-09-25 17:09:45 +0300

    goToCrmLead_is_removed

--- crm_lead_add/index.php
+++ crm_lead_add/index.php
@@ -4,12 +4,12 @@ require_once (__DIR__.'/crest.php');
 //that was working:
 $result = 'hello world';
 
-/*
+
 $result = CRest::call(
 		'crm.lead.add',
 		{{query[params][PARAMS]}}
 	);
-*/
+
 
 
 
---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+
+
+    <section class="bitrix_tests">
+        <a href="/crm_lead_add/index.php">go to crm lead add</a>
+        <a href="./crm_contact_add/index.php">go to crm contact add</a>
+    </section>
+
+
+
+
+
+
+
+</body>
+</html>
